236fdca5ffeafffb29a2f04a269619f6d46f1b51
barış arslan <barisfiratarslan@hotmail.com>
2021-09-17 17:21:16 +0300

    Updated 17.09.21

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        void CatgeoryAdd(Category category);
+        void CatgeoryDelete(Category category);
+        void CatgeoryUpdate(Category category);
+        List<Category> GetList();
+        Category GetByID(int id);
+    }
+}
