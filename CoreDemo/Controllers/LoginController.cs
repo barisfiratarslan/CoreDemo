e24f88c303eba18304258ad5cf56f95ac4aaf054
barış arslan <barisfiratarslan@hotmail.com>
2021-11-01 00:00:22 +0300

    Updated 08.10.21

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,10 +10,25 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            Context context = new Context();
+            var data = context.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
+            if (data != null)
+            {
+                HttpContext.Session.SetString("username", writer.WriterMail);
+                return RedirectToAction("Index", "Blog");
+            }
             return View();
         }
     }
